de563921fafdfa0b5ada8f744489140eb905d273
Gilbert Dim <ark.gdimaano@gmail.com>
2024-03-13 08:53:27 +0800

    add website title

--- resources/js/vue/Shared/Header.vue
+++ resources/js/vue/Shared/Header.vue
@@ -1,10 +1,14 @@
 <template>
   <header>
+    <title v-text="props.site.name"></title>
   </header>
 </template>
 
 <script setup>
 
+import {usePage} from "@inertiajs/inertia-vue3";
+
+const { props } = usePage();
 </script>
 
 <style>
